476af6e9c0b09c71e45fce654300596c6a27bbb7
Pierre Roudier <pierre.roudier@gmail.com>
2023-08-30 14:27:26 +1200

    Initial, simple version of the package. Just the read part for now!

---
+++ R/read_sqlite.R
@@ -0,0 +1,25 @@
+#' @title Read a SQLite DB into a data.frame
+#'
+#' @param fn Path to a SQLite file
+#'
+#' @returns a data.frame
+#'
+#' @author Pierre Roudier
+#'
+#' @importFrom RSQLite SQLite dbConnect dbReadTable dbDisconnect
+#' @export
+#'
+read_sqlite <- function(fn) {
+
+  # Initiate connection to SQLite
+  con <- dbConnect(SQLite(), fn)
+
+  # Read data View
+  df <- dbReadTable(con, "MfE_Carbon_data")
+
+  # Close connection
+  dbDisconnect(con)
+
+  # Returns data.frame
+  return(df)
+}
