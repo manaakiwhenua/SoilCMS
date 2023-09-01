6a8231541fbda2a499fb10ed7c9ec47619fd5bf8
Pierre Roudier <pierre.roudier@gmail.com>
2023-09-01 17:22:38 +1200

    Started implementing stocks calculation alternatives

--- R/process_mfe_data.R
+++ R/process_mfe_data.R
@@ -4,29 +4,79 @@
 #' @returns a data.frame with two additional columns for SOC and total N stocks
 #'
 calculate_stocks <- function(df) {
+
   #
   # CALCULATED FIELDS
   #
 
-  # Total oven dry sample weight
-  df$amt_coarse_airdry_g = df$amt_coarse_airdry_gv # Typo in the VIEW
-  df$amt_total_oven_dry_sample_g = df$amt_coarse_airdry_g +
-    (df$amt_sample_airdry_g - df$amt_coarse_airdry_g) / (1 + df$amt_airdry_water_content_p / 100)
+  # Typo in the VIEW, this is a temporary fix
+  df$amt_coarse_airdry_g = df$amt_coarse_airdry_gv
 
-  # Gravimetric content of coarse fraction
-  df$amt_calc_coarse_fraction_pp = df$amt_coarse_airdry_g / df$amt_total_oven_dry_sample_g
+  # Test to see if we have the bare necessities
+  if (
+    "amt_orgc_p" %in% names(df) &
+    "amt_tn_p" %in% names(df) &
+    "amt_sample_depth_lower_cm" %in% names(df) &
+    "amt_sample_depth_upper_cm" %in% names(df)
+  ) {
+    stop("Can't calculate SOC and TN stocks due to missing information about soil depth and carbon/nitrogen concentrations.", call. = FALSE)
+  }
 
-  # total SOC stocks
-  df$amt_calc_orgc_mgha = df$amt_orgc_p *
-    df$amt_bulkdensity_total_gcm3 *
-    (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm) *
-    (1 - df$amt_calc_coarse_fraction_pp)
+  if (
+    "amt_coarse_airdry_g" %in% names(df) &
+    "amt_sample_airdry_g" %in% names(df) &
+    "amt_airdry_water_content_p" %in% names(df) &
+    "amt_bulkdensity_total_gcm3" %in% names(df)
+  ) {
 
-  # Total nitrogen stocks
-  df$amt_calc_tn_mgha = df$amt_tn_p *
-    df$amt_bulkdensity_total_gcm3 *
-    (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm) *
-    (1 - df$amt_calc_coarse_fraction_pp)
+    # Total oven dry sample weight
+    df$amt_total_oven_dry_sample_g = df$amt_coarse_airdry_g +
+      (df$amt_sample_airdry_g - df$amt_coarse_airdry_g) / (1 + df$amt_airdry_water_content_p / 100)
+
+    # Gravimetric content of coarse fraction
+    df$amt_calc_coarse_fraction_pp = df$amt_coarse_airdry_g / df$amt_total_oven_dry_sample_g
+
+    # total SOC stocks
+    df$amt_calc_orgc_mgha = df$amt_orgc_p *
+      df$amt_bulkdensity_total_gcm3 *
+      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm) *
+      (1 - df$amt_calc_coarse_fraction_pp)
+
+    # Total nitrogen stocks
+    df$amt_calc_tn_mgha = df$amt_tn_p *
+      df$amt_bulkdensity_total_gcm3 *
+      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm) *
+      (1 - df$amt_calc_coarse_fraction_pp)
+
+  } else if (
+    "amt_bulkdensity_of_2mm_per_tot_sample_volume_gcm3" %in% names(df)
+  ) {
+    # total SOC stocks
+    df$amt_calc_orgc_mgha = df$amt_orgc_p *
+      df$amt_bulkdensity_of_2mm_per_tot_sample_volume_gcm3 *
+      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm)
+
+    # Total nitrogen stocks
+    df$amt_calc_tn_mgha = df$amt_tn_p *
+      df$amt_bulkdensity_of_2mm_per_tot_sample_volume_gcm3 *
+      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm)
+  } else if(
+    "amt_bulkdensity_total2_gcm3"
+  ) {
+    # total SOC stocks
+    df$amt_calc_orgc_mgha = df$amt_orgc_p *
+      (df$amt_total_oven_dry_sample2_g / amt_sampled_volume_cm3) *
+      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm) *
+      (1 - df$amt_calc_coarse_fraction_pp)
+
+    # Total nitrogen stocks
+    df$amt_calc_tn_mgha = df$amt_tn_p *
+      (df$amt_total_oven_dry_sample2_g / amt_sampled_volume_cm3) *
+      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm) *
+      (1 - df$amt_calc_coarse_fraction_pp)
+  } else {
+    stop("Can't calculate SOC and TN stocks due to missing columns.", call. = FALSE)
+  }
 
   return(df)
 }
