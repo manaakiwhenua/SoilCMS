079503157f2bb1f9cd47d25cdec4b631a5246204
Pierre Roudier <pierre.roudier@gmail.com>
2023-09-07 16:42:40 +1200

    Implemented carbon + nitrogen stocks calculation algorithms

--- R/process_mfe_data.R
+++ R/process_mfe_data.R
@@ -1,89 +1,106 @@
+#' @noRd
+.calculate_total_dry_weight <- function(
+    amt_sample_airdry_g,
+    amt_coarse_airdry_g,
+    amt_airdry_water_content_p,
+    amt_field_moist_water_content_p,
+    amt_sample_wet_g
+  ) {
+
+  if (!is.na(amt_coarse_airdry_g) & !is.na(amt_sample_airdry_g) & !is.na(amt_airdry_water_content_p)) {
+    # Total oven dry sample weight
+    total_dry_weight <- amt_coarse_airdry_g + (amt_sample_airdry_g - amt_coarse_airdry_g) / (1 + amt_airdry_water_content_p / 100)
+  } else if (!is.na(amt_field_moist_water_content_p) & !is.na(amt_sample_wet_g)) {
+    # ALTERNATIVE way to compute total dry_weight
+    # Calculate moisture factor
+    moisture_factor <- 1 + amt_field_moist_water_content_p / 100
+    total_dry_weight <-  amt_sample_wet_g / moisture_factor
+  } else {
+    total_dry_weight <- NA
+  }
+
+  return(total_dry_weight)
+}
+
+#' @noRd
+.calculate_bd_fines <- function(
+  # FIELDS USED IN BD FINES COMPUTATION
+  #
+  # - amt_sample_airdry_g
+  # - amt_coarse_airdry_g
+  # - amt_airdry_water_content_p
+  # - amt_sampled_volume_cm3
+  # - amt_bulkdensity_of_2mm_per_tot_sample_volume_gcm3
+  amt_sampled_volume_cm3,
+  amt_sample_airdry_g,
+  amt_coarse_airdry_g,
+  amt_airdry_water_content_p,
+  amt_field_moist_water_content_p,
+  amt_sample_wet_g,
+  amt_fine_od_g
+  ) {
+
+  if(!is.na(amt_fine_od_g)) {
+    # If we have the oven dry weight of fine fraction
+    fine_bulk_density <- amt_fine_od_g / amt_sampled_volume_cm3
+  } else {
+    # Otherwise we have to use one of 2 methods
+    # to compute the total dry weight
+    total_dry_weight <- .calculate_total_dry_weight(
+      amt_sample_airdry_g,
+      amt_coarse_airdry_g,
+      amt_airdry_water_content_p,
+      amt_field_moist_water_content_p,
+      amt_sample_wet_g
+    )
+
+    # Calculate total BD
+    total_bulk_density <- total_dry_weight / amt_sampled_volume_cm3
+
+    # Calculate coarse fraction
+    coarse_fraction <- amt_coarse_airdry_g / total_dry_weight
+    fine_fraction <- 1 - coarse_fraction
+
+    # Calculate BD of fine fraction
+    fine_bulk_density <- total_bulk_density * fine_fraction
+  }
+
+  return(fine_bulk_density)
+}
+
+
 #' Calculate Carbon and Nitrogen Stocks
 #'
 #' @param df a data.frame read from the NSDR Viewer SQLite
+#'
 #' @returns a data.frame with two additional columns for SOC and total N stocks
 #'
+#' @author Pierre Roudier
+#'
+#' @importFrom dplyr mutate
+#' @export
 calculate_stocks <- function(df) {
 
-  #
-  # CALCULATED FIELDS
-  #
+  res <- mutate(df,
+    amt_sample_depth_upper_cm = lab_samplingdepth_minval,
+    amt_sample_depth_lower_cm = lab_samplingdepth_maxval,
+    # amt_fine_od_g = NA,
+    # amt_sampled_volume_cm3 = 1152,
+    thickness = abs(amt_sample_depth_lower_cm - amt_sample_depth_upper_cm),
+    fine_bulk_density = .calculate_bd_fines(
+      amt_sampled_volume_cm3,
+      amt_sample_airdry_g,
+      amt_coarse_airdry_g,
+      amt_airdry_water_content_p,
+      amt_field_moist_water_content_p,
+      amt_sample_wet_g,
+      amt_fine_od_g
+    ),
+    carbon_stocks = amt_orgc_p * thickness * fine_bulk_density,
+    nitrogen_stocks = amt_tn_p * thickness * fine_bulk_density
+  )
 
-  # Typo in the VIEW, this is a temporary fix
-  if (
-    (! "amt_coarse_airdry_g" %in% names(df)) &
-    "amt_coarse_airdry_gv" %in% names(df)
-  ) {
-    df$amt_coarse_airdry_g = df$amt_coarse_airdry_gv
-  }
-
-  # Test to see if we have the bare necessities
-  if (
-    "amt_orgc_p" %in% names(df) &
-    "amt_tn_p" %in% names(df) &
-    "amt_sample_depth_lower_cm" %in% names(df) &
-    "amt_sample_depth_upper_cm" %in% names(df)
-  ) {
-    stop("Can't calculate SOC and TN stocks due to missing information about soil depth and carbon/nitrogen concentrations.", call. = FALSE)
-  }
-
-  if (
-    "amt_coarse_airdry_g" %in% names(df) &
-    "amt_sample_airdry_g" %in% names(df) &
-    "amt_airdry_water_content_p" %in% names(df) &
-    "amt_bulkdensity_total_gcm3" %in% names(df)
-  ) {
-
-    # Total oven dry sample weight
-    df$amt_total_oven_dry_sample_g = df$amt_coarse_airdry_g +
-      (df$amt_sample_airdry_g - df$amt_coarse_airdry_g) / (1 + df$amt_airdry_water_content_p / 100)
-
-    # Gravimetric content of coarse fraction
-    df$amt_calc_coarse_fraction_pp = df$amt_coarse_airdry_g / df$amt_total_oven_dry_sample_g
-
-    # total SOC stocks
-    df$amt_calc_orgc_mgha = df$amt_orgc_p *
-      df$amt_bulkdensity_total_gcm3 *
-      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm) *
-      (1 - df$amt_calc_coarse_fraction_pp)
-
-    # Total nitrogen stocks
-    df$amt_calc_tn_mgha = df$amt_tn_p *
-      df$amt_bulkdensity_total_gcm3 *
-      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm) *
-      (1 - df$amt_calc_coarse_fraction_pp)
-
-  } else if (
-    "amt_bulkdensity_of_2mm_per_tot_sample_volume_gcm3" %in% names(df)
-  ) {
-    # total SOC stocks
-    df$amt_calc_orgc_mgha = df$amt_orgc_p *
-      df$amt_bulkdensity_of_2mm_per_tot_sample_volume_gcm3 *
-      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm)
-
-    # Total nitrogen stocks
-    df$amt_calc_tn_mgha = df$amt_tn_p *
-      df$amt_bulkdensity_of_2mm_per_tot_sample_volume_gcm3 *
-      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm)
-  } else if(
-    "amt_bulkdensity_total2_gcm3"
-  ) {
-    # total SOC stocks
-    df$amt_calc_orgc_mgha = df$amt_orgc_p *
-      (df$amt_total_oven_dry_sample2_g / amt_sampled_volume_cm3) *
-      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm) *
-      (1 - df$amt_calc_coarse_fraction_pp)
-
-    # Total nitrogen stocks
-    df$amt_calc_tn_mgha = df$amt_tn_p *
-      (df$amt_total_oven_dry_sample2_g / amt_sampled_volume_cm3) *
-      (df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm) *
-      (1 - df$amt_calc_coarse_fraction_pp)
-  } else {
-    stop("Can't calculate SOC and TN stocks due to missing columns.", call. = FALSE)
-  }
-
-  return(df)
+  return(res)
 }
 
 #' Process the Downloaded Data
