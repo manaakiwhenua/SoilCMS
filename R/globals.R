b650e06cd2aad7dbf7d15899b4c9f5e30f81e996
Pierre Roudier <pierre.roudier@gmail.com>
2023-09-12 14:44:30 +1200

    added global variables

---
+++ R/globals.R
@@ -0,0 +1,16 @@
+#' @noRd
+amt_airdry_water_content_p <- amt_coarse_airdry_g <-
+  amt_field_moist_water_content_p <-
+  amt_fine_od_g <-
+  amt_orgc_p <-
+  amt_sample_airdry_g <-
+  amt_sample_depth_lower_cm <-
+  amt_sample_depth_upper_cm <-
+  amt_sample_wet_g <-
+  amt_sampled_volume_cm3 <-
+  amt_tn_p <-
+  fine_bulk_density <-
+  lab_samplingdepth_maxval <-
+  lab_samplingdepth_minval <-
+  thickness <-
+    NULL
