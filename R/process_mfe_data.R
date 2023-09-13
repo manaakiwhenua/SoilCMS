bd46dc3964439fbbfe5dd89ed9258dc66b2da249
Pierre Roudier <pierre.roudier@gmail.com>
2023-09-13 16:35:11 +1200

    Updated stocks calculation so it finally runs. Will still have to validate those results, though.

--- R/process_mfe_data.R
+++ R/process_mfe_data.R
@@ -1,26 +1,26 @@
 #' @noRd
-.calculate_total_dry_weight <- function(
-    amt_sample_airdry_g,
-    amt_coarse_airdry_g,
-    amt_airdry_water_content_p,
-    amt_field_moist_water_content_p,
-    amt_sample_wet_g
-  ) {
-
-  if (!is.na(amt_coarse_airdry_g) & !is.na(amt_sample_airdry_g) & !is.na(amt_airdry_water_content_p)) {
-    # Total oven dry sample weight
-    total_dry_weight <- amt_coarse_airdry_g + (amt_sample_airdry_g - amt_coarse_airdry_g) / (1 + amt_airdry_water_content_p / 100)
-  } else if (!is.na(amt_field_moist_water_content_p) & !is.na(amt_sample_wet_g)) {
-    # ALTERNATIVE way to compute total dry_weight
-    # Calculate moisture factor
-    moisture_factor <- 1 + amt_field_moist_water_content_p / 100
-    total_dry_weight <-  amt_sample_wet_g / moisture_factor
-  } else {
-    total_dry_weight <- NA
-  }
-
-  return(total_dry_weight)
-}
+# .calculate_total_dry_weight <- function(
+#     amt_sample_airdry_g,
+#     amt_coarse_airdry_g,
+#     amt_airdry_water_content_p,
+#     amt_field_moist_water_content_p,
+#     amt_sample_wet_g
+#   ) {
+#
+#   if (!is.na(amt_coarse_airdry_g) & !is.na(amt_sample_airdry_g) & !is.na(amt_airdry_water_content_p)) {
+#     # Total oven dry sample weight
+#     total_dry_weight <- amt_coarse_airdry_g + (amt_sample_airdry_g - amt_coarse_airdry_g) / (1 + amt_airdry_water_content_p / 100)
+#   } else if (!is.na(amt_field_moist_water_content_p) & !is.na(amt_sample_wet_g)) {
+#     # ALTERNATIVE way to compute total dry_weight
+#     # Calculate moisture factor
+#     moisture_factor <- 1 + amt_field_moist_water_content_p / 100
+#     total_dry_weight <-  amt_sample_wet_g / moisture_factor
+#   } else {
+#     total_dry_weight <- NA
+#   }
+#
+#   return(total_dry_weight)
+# }
 
 #' @noRd
 .calculate_bd_fines <- function(
@@ -35,7 +35,7 @@
   amt_sample_airdry_g,
   amt_coarse_airdry_g,
   amt_airdry_water_content_p,
-  amt_field_moist_water_content_p,
+  # amt_field_moist_water_content_p,
   amt_sample_wet_g,
   amt_fine_od_g
   ) {
@@ -46,13 +46,17 @@
   } else {
     # Otherwise we have to use one of 2 methods
     # to compute the total dry weight
-    total_dry_weight <- .calculate_total_dry_weight(
-      amt_sample_airdry_g,
-      amt_coarse_airdry_g,
-      amt_airdry_water_content_p,
-      amt_field_moist_water_content_p,
-      amt_sample_wet_g
-    )
+    # total_dry_weight <- .calculate_total_dry_weight(
+    #   amt_sample_airdry_g,
+    #   amt_coarse_airdry_g,
+    #   amt_airdry_water_content_p,
+    #   amt_field_moist_water_content_p,
+    #   amt_sample_wet_g
+    # )
+
+    total_dry_weight <- amt_coarse_airdry_g +
+      (amt_sample_airdry_g - amt_coarse_airdry_g) /
+      (1 + amt_airdry_water_content_p / 100)
 
     # Calculate total BD
     total_bulk_density <- total_dry_weight / amt_sampled_volume_cm3
@@ -68,7 +72,6 @@
   return(fine_bulk_density)
 }
 
-
 #' Calculate Carbon and Nitrogen Stocks
 #'
 #' @param df a data.frame read from the NSDR Viewer SQLite
@@ -83,31 +86,79 @@
 #'
 #' @author Pierre Roudier
 #'
-#' @importFrom dplyr mutate
 #' @export
 calculate_stocks <- function(df) {
 
-  res <- mutate(df,
-    amt_sample_depth_upper_cm = lab_samplingdepth_minval,
-    amt_sample_depth_lower_cm = lab_samplingdepth_maxval,
-    # amt_fine_od_g = NA,
-    # amt_sampled_volume_cm3 = 1152,
-    thickness = abs(amt_sample_depth_lower_cm - amt_sample_depth_upper_cm),
-    fine_bulk_density = .calculate_bd_fines(
-      amt_sampled_volume_cm3,
-      amt_sample_airdry_g,
-      amt_coarse_airdry_g,
-      amt_airdry_water_content_p,
-      amt_field_moist_water_content_p,
-      amt_sample_wet_g,
-      amt_fine_od_g
-    ),
-    carbon_stocks = amt_orgc_p * thickness * fine_bulk_density,
-    nitrogen_stocks = amt_tn_p * thickness * fine_bulk_density
+  # Finally, calculate stocks
+  df$carbon_stocks <- df$amt_orgc_p * df$thickness * df$fine_bulk_density
+  df$nitrogen_stocks <- df$amt_tn_p * df$thickness * df$fine_bulk_density
+
+  return(df)
+}
+
+#' Calculate the bulk density of the fine fraction
+#'
+#' @param df a data.frame read from the NSDR Viewer SQLite
+#' @returns a data.frame with added/processed data
+#'
+calculate_fine_bd <- function(df) {
+  df$fine_bulk_density <- do.call(
+    c,
+    plyr::alply(
+      df,
+      1,
+      function(x) {
+
+        res <- .calculate_bd_fines(
+          amt_sampled_volume_cm3 = x$amt_sampled_volume_cm3,
+          amt_sample_airdry_g = x$amt_sample_airdry_g,
+          amt_coarse_airdry_g = x$amt_coarse_airdry_g,
+          amt_airdry_water_content_p = x$amt_airdry_water_content_p,
+          amt_sample_wet_g = x$amt_sample_wet_g,
+          amt_fine_od_g = x$amt_fine_od_g
+        )
+
+        return(res)
+      }
+    )
   )
 
-  return(res)
+  return(df)
 }
+
+#' Calculate sample volume
+#'
+#' @param df a data.frame read from the NSDR Viewer SQLite
+#' @returns a data.frame with added/processed data
+#' @importFrom dplyr mutate case_when
+calculate_volume <- function(df) {
+  # PI()*(amt_core_diameter_cm/2)^2*(thickness)*n_composite
+  df <- mutate(
+      df,
+      amt_sampled_volume_cm3 = case_when(
+        is.na(amt_sampled_volume_cm3) ~ pi * (amt_core_diameter_cm_val/2)^2 * (thickness) * n_composite,
+        TRUE ~ amt_sampled_volume_cm3
+      )
+    )
+
+  return(df)
+}
+
+#' Calculate sample thickness
+#'
+#' @param df a data.frame read from the NSDR Viewer SQLite
+#' @returns a data.frame with added/processed data
+#'
+calculate_thickness <- function(df) {
+
+  # Calculate thickness
+  df$amt_sample_depth_upper_cm <- df$lab_samplingdepth_minval
+  df$amt_sample_depth_lower_cm <- df$lab_samplingdepth_maxval
+  df$thickness <- abs(df$amt_sample_depth_lower_cm - df$amt_sample_depth_upper_cm)
+
+  return(df)
+}
+
 
 #' Process the Downloaded Data
 #'
@@ -115,6 +166,15 @@ calculate_stocks <- function(df) {
 #' @returns a data.frame with added/processed data
 #' @export
 process_mfe_data <- function(df) {
+
+  # Calculate sample thickness
+  df <- calculate_thickness(df)
+
+  # Calculate total sample volume
+  df <- calculate_volume(df)
+
+  # Calculate bulk density of fine fraction
+  df <- calculate_fine_bd(df)
 
   # Add SOC and TN stocks
   df <- calculate_stocks(df)
