114c674bc1c68091e506330e385e9f9209a28753
Pierre Roudier <pierre.roudier@gmail.com>
2023-09-18 12:11:32 +1200

    now exporting read function

--- R/read_mfe_sqlite.R
+++ R/read_mfe_sqlite.R
@@ -7,7 +7,7 @@
 #' @author Pierre Roudier
 #'
 #' @importFrom RSQLite SQLite dbConnect dbReadTable dbDisconnect
-#'
+#' @export
 read_mfe_sqlite <- function(fn) {
 
   # Initiate connection to SQLite
