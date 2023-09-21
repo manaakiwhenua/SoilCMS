a78c94abf732d6bdd593a6dc383b0d025a9803c4
Pierre Roudier <pierre.roudier@gmail.com>
2023-09-21 15:58:14 +1200

    small update on the way names are calculated for export

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,9 +1,32 @@
-df_config <- openxlsx::read.xlsx("data-raw/Soil_carbon_template.config.xlsx", sheet = "Columns")
-names_config <- data.frame(
-  short_heading = df_config[,1],
-  long_heading = df_config[,2]
-)
-names_config$short_heading <- stringr::str_replace(names_config$short_heading, ">", "_")
-names_config$short_heading <- stringr::str_replace(names_config$short_heading, "<", "_")
+config_sampling_site <- openxlsx::read.xlsx("data-raw/Soil carbon template. MfE CMS project.xlsx", sheet = "Sampling site details")
+config_sample <- openxlsx::read.xlsx("data-raw/Soil carbon template. MfE CMS project.xlsx", sheet = "Sample data")
+config_landuse <- openxlsx::read.xlsx("data-raw/Soil carbon template. MfE CMS project.xlsx", sheet = "Landuse details")
 
-usethis::use_data(names_config, internal = TRUE)
+df_config_sampling_site <- data.frame(
+  short_heading = names(config_sampling_site),
+  long_heading = as.character(config_sampling_site[1,])
+)
+df_config_sample <- data.frame(
+  short_heading = names(config_sample),
+  long_heading = as.character(config_sample[1,])
+)
+df_config_landuse <- data.frame(
+  short_heading = names(config_landuse),
+  long_heading = as.character(config_landuse[1,])
+)
+
+df_config <- rbind(
+  df_config_sampling_site,
+  df_config_sample,
+  df_config_landuse
+)
+
+# df_config <- openxlsx::read.xlsx("data-raw/Soil_carbon_template.config.xlsx", sheet = "Columns")
+# names_config <- data.frame(
+#   short_heading = df_config[,1],
+#   long_heading = df_config[,2]
+# )
+names_config$short_heading <- stringr::str_replace(names_config$short_heading, ">", "")
+names_config$short_heading <- stringr::str_replace(names_config$short_heading, "<", "")
+
+usethis::use_data(names_config, internal = TRUE, overwrite = TRUE)
